3c9bfd6322883527255513f68852ff8f30a63acd
Igor Dossin Lazzaretti <134664486+igorlazzaretti@users.noreply.github.com>
2025-01-02 23:12:43 -0300

    :man_technologist: Hogwarts School with Adianti 8 Update: cadastrado botões na DataGrid: Excluir

--- app/control/AlunosTotal.class.php
+++ app/control/AlunosTotal.class.php
@@ -1,6 +1,8 @@
 <?php
 
+use Adianti\Control\TAction;
 use Adianti\Control\TPage;
+use Adianti\Core\AdiantiCoreTranslator;
 use Adianti\Database\TDatabase;
 use Adianti\Database\TTransaction;
 use Adianti\Widget\Container\TPanelGroup;
@@ -9,6 +11,8 @@ use Adianti\Widget\Datagrid\TDataGrid;
 use Adianti\Widget\Datagrid\TDataGridAction;
 use Adianti\Widget\Datagrid\TDataGridColumn;
 use Adianti\Widget\Dialog\TMessage;
+use Adianti\Widget\Dialog\TQuestion;
+use Adianti\Widget\Dialog\TToast;
 use Adianti\Widget\Util\TXMLBreadCrumb;
 use Adianti\Wrapper\BootstrapDatagridWrapper;
 
@@ -24,10 +28,11 @@ class AlunosTotal extends TPage
         $this->datagrid = new BootstrapDatagridWrapper(new TDataGrid);
 
         // create the datagrid columns
-        $name  = new TDataGridColumn( 'name',  'Bruxo(a)',    'left',   '25%');
-        $age  = new TDataGridColumn(  'age',   'Idade',       'left',   '25%');
-        $year  = new TDataGridColumn( 'year',  'Ano Escolar', 'left',   '25%');
-        $house  = new TDataGridColumn('house', 'Casa',        'left',   '25%');
+        $id = new TDataGridColumn('id', 'Nro', 'left', '10%');
+        $name  = new TDataGridColumn  ( 'name',  'Bruxo(a)',    'left',   '25%');
+        $age  = new TDataGridColumn   ( 'age',   'Idade',       'left',   '20%');
+        $year  = new TDataGridColumn  ( 'year',  'Ano Escolar', 'left',   '20%');
+        $house  = new TDataGridColumn ( 'house', 'Casa',        'left',   '25%');
 
 
         // add the columns to the datagrid, with actions on column titles, passing parameters
@@ -38,23 +43,36 @@ class AlunosTotal extends TPage
 
         // creates two datagrid actions
         $action1 = new TDataGridAction([$this, 'onView'],[
-            'nome'=>'{name}',
+            'id'    => '{id}',
+            'nome'  => '{name}',
             'idade' => '{age}',
-            'casa'=> '{house}',
-            'ano' => '{year}'
+            'casa'  => '{house}',
+            'ano'   => '{year}'
         ]);
         $action2 = new TDataGridAction([$this, 'onSubject'],[
-            'nome'=>'{name}',
-            'ano' => '{year}'
+            'nome' => '{name}',
+            'ano'  => '{year}'
+        ]);
+        $action3 = new TDataGridAction([$this, 'onDelete'],[
+            'id'    => '{id}',
+            'nome'  => '{name}',
+        ]);
+        $action4 = new TDataGridAction([$this, 'onEdit'],[
+            'id'    => '{id}',
+            'nome'  => '{name}',
         ]);
 
         // custom button presentation
         $action1->setUseButton(TRUE);
         $action2->setUseButton(TRUE);
+        $action3->setUseButton(TRUE);
+        $action4->setUseButton(TRUE);
 
         // add the actions to the datagrid
-        $this->datagrid->addAction($action1, 'Informações', 'fa:search blue');
-        $this->datagrid->addAction($action2, 'Matérias', 'fa:book blue');
+        $this->datagrid->addAction($action1, '', 'fa:search blue');
+        $this->datagrid->addAction($action2, 'Matérias', 'fa:book purple');
+        $this->datagrid->addAction($action3, '', 'fa:trash red');
+        $this->datagrid->addAction($action4, '', 'fa:edit green');
 
 
         // creates the datagrid model
@@ -71,6 +89,7 @@ class AlunosTotal extends TPage
             foreach ($result as $row)
             {
                 $item = new StdClass;
+                $item->id = $row['id'];
                 $item->name = $row['nome'];
                 $item->age = $row['idade'];
                 $item->year = $row['ano'];
@@ -149,5 +168,83 @@ class AlunosTotal extends TPage
         } finally {
             TTransaction::close();
         }
-}
+    }
+    public static function onDelete($param)
+    {
+        // define the delete action
+        $action = new TAction(array(__CLASS__, 'Delete'));
+        $action->setParameters($param); // pass the key parameter ahead
+
+        $name   = $param['name'];
+        // shows a dialog to the user
+        new TQuestion(('Quer mesmo deletar ' . $name . '?'), $action);
+
+    }
+
+    /**
+     * Delete a record
+     */
+    public function Delete($param)
+    {
+        try
+        {
+            $key = $param['key']; // get the parameter $key
+            TTransaction::open('hogwartsdb'); // open a transaction with database
+            $conn = TTransaction::get(); // get the database connection
+
+            // executa a query SQL para deletar o aluno
+            $conn->exec("DELETE FROM aluno WHERE id = {$key}");
+
+            TTransaction::close(); // close the transaction
+
+            $pos_action = new TAction([__CLASS__, 'onReload']);
+            new TMessage('info', AdiantiCoreTranslator::translate('Record deleted'), $pos_action); // success message
+
+            TToast::show('warning', 'Aluno deletado com sucesso!', 'bottom right', 'far:check-circle');
+
+            // Chama o método onReload para recarregar a lista
+            self::onReload();
+        } catch (Exception $e) // in case of exception
+        {
+            new TMessage('error', $e->getMessage()); // shows the exception error message
+            TTransaction::rollback(); // undo all pending operations
+        }
+    }
+
+    public function onEdit() {
+        new TMessage('info', 'Editando');
+    }
+
+    public function onReload($param = null)
+    {
+
+        // creates the datagrid model
+        $this->datagrid->clear();
+
+        // Start Populatin Data
+        try {
+            TTransaction::open('hogwartsdb');
+            $conn = TTransaction::get();
+
+            $result = $conn->query('SELECT
+                id, nome, idade, casa, ano FROM aluno ORDER BY id');
+
+            foreach ($result as $row)
+            {
+                $item = new StdClass;
+                $item->id = $row['id'];
+                $item->name = $row['nome'];
+                $item->age = $row['idade'];
+                $item->year = $row['ano'];
+                $item->house = $row['casa'];
+                $this->datagrid->addItem($item);
+            }
+
+            TTransaction::close();
+
+        } catch (Exception $e) {
+            new TMessage('error', $e->getMessage());
+        }
+
+    }
 }
