815b2633c6718f7af0ab436ec7cee4d5171fd13c
Igor Lazzaretti <igor.lazzaretti@plenatech.com>
2025-01-22 15:28:23 -0300

    :european_castle: Hogwarts School with Adianti 8 Update: Editando as Matérias Podemos adicionar professores.

--- app/control/Matérias/MateriasEdit.php
+++ app/control/Matérias/MateriasEdit.php
@@ -36,9 +36,13 @@ class MateriasEdit extends TWindow
         $nome = new TEntry('nome');
         $ano = new TCombo('ano');
         $assunto = new TText('assunto');
+        $professor_atual = new TEntry('professor_atual');
+        $professor_atual->setEditable(false);
+
+
+        $professor = new TCombo('professor');
 
         $id->setEditable(false);
-
         // Adiciona opções para o campo 'ano'
         $ano->addItems([
             1 => '1º Ano',
@@ -48,14 +52,32 @@ class MateriasEdit extends TWindow
         ]);
 
         $ano->setValue('1');
-        $id->setEditable(false);
 
-        $this->form->addRowField('Número de Identificação',   $id,   true);
-        $this->form->addRowField('Nome', $nome, true);
-        $this->form->addRowField('Ano',  $ano,  true);
-        $this->form->addRowField('Assunto',  $assunto,  true);
+        try {
+            TTransaction::open('hogwartsdb');
+            $professores = Professor::getObjects(); // Retrieves all Materia records
 
-        $this->form->addAction('Salvar', new TAction([$this, 'onSave']), 'fa:save');
+            $items = [];
+            foreach ($professores as $m) {
+                $items[$m->id] = $m->nome; // Key is ID, Value is Nome
+            }
+            $professor->addItems($items);
+
+            TTransaction::close();
+
+        } catch (Exception $e) {
+            new TMessage('error', $e->getMessage());
+            TTransaction::rollback();
+        }
+
+        $this->form->addRowField('Número de Identificação', $id,        true);
+        $this->form->addRowField('Nome',                    $nome,      true);
+        $this->form->addRowField('Ano',                     $ano,       true);
+        $this->form->addRowField('Assunto',                 $assunto,   true);
+        $this->form->addRowField('Professor Atual:',        $professor_atual, true);
+        $this->form->addRowField('Professor',               $professor,       true);
+
+        $this->form->addAction(      'Salvar', new TAction([$this, 'onSave']),    'fa:save');
         $this->form->addFooterAction('Voltar', new TAction([$this, 'onSuccess']), 'fa:arrow-left');
 
         parent::add($this->form);
@@ -79,14 +101,43 @@ class MateriasEdit extends TWindow
                     $this->form->setData($materia); // preenche o formulário com os dados do aluno
                 }
 
-                TTransaction::close(); // fecha a transação
+            }  // SETVALUE
+            // Obtém o ID da matéria a partir dos parâmetros
+            $materia_id = $param['id'];
+
+            $conn = TTransaction::get(); // obtém a conexão ativa
+
+            $stmt = $conn->prepare('SELECT nome FROM Professor WHERE materia_id = :materia_id');
+            $stmt->execute([':materia_id' => $materia_id]);
+
+            $professores = $stmt->fetchAll();
+
+            if ($professores) {
+                $nomes_professores = '';
+                foreach ($professores as $professor) {
+                    $nomes_professores .= $professor['nome'] . ', ';
+                }
+                $nomes_professores = rtrim($nomes_professores, ', '); // Remove trailing comma and space
+
+                // Adiciona $nomes_professores ao campo 'professor_atual'
+                $professor_atual = $this->form->getField('professor_atual');
+                $professor_atual->setValue($nomes_professores);
+
             }
+
+
+            TTransaction::close(); // fecha a transação
+
         } catch (Exception $e) {
             new TMessage('error', $e->getMessage());
             TTransaction::rollback(); // desfaz a transação em caso de erro
         }
     }
 
+    /**
+     *  Método onSave()
+     *  Salva os dados do formulário
+     */
     public function onSave($param)
     {
         try {
@@ -96,9 +147,20 @@ class MateriasEdit extends TWindow
 
             $data = $this->form->getData(); // obtém os dados do formulário
 
+
             $materia = new Materia;
             $materia->fromArray((array) $data);
             $materia->store(); // armazena o aluno no banco de dados
+
+            var_dump($data); // OK pega os dados do formulário
+            $conn = TTransaction::get();
+            // ID do Professor
+            $data_professor = $data->professor;
+            // ID da Matéria
+            $data_materia = $data->id;
+            // Atualiza o ID da Matéria na tabela Professor
+            $stmt = $conn->prepare('UPDATE Professor SET materia_id = :materia_id WHERE id = :professor_id');
+            $stmt->execute([':materia_id' => $data_materia, ':professor_id' => $data_professor]);
 
             TTransaction::close(); // fecha a transação
 
@@ -110,9 +172,52 @@ class MateriasEdit extends TWindow
             TTransaction::rollback(); // desfaz a transação em caso de erro
         }
     }
+
+    /**
+     *  Método onSuccess()
+     *  Redireciona para a página de listagem de alunos
+     */
     public function onSuccess()
     {
         AdiantiCoreApplication::gotoPage('Materias', 'onReload');
     }
+
+    /**
+     *  Método getProfessor()
+     */
+    public static function getProfessor($param)
+    {
+        try {
+            TTransaction::open('hogwartsdb'); // abre uma transação com o banco de dados
+
+            // Obtém o ID da matéria a partir dos parâmetros
+            $materia_id = $param['id'];
+
+            $conn = TTransaction::get(); // obtém a conexão ativa
+
+            $stmt = $conn->prepare('SELECT nome FROM Professor WHERE materia_id = :materia_id');
+            $stmt->execute([':materia_id' => $materia_id]);
+
+            $professores = $stmt->fetchAll();
+
+            if ($professores) {
+                $nomes_professores = '';
+                foreach ($professores as $professor) {
+                    $nomes_professores .= $professor['nome'] . ', ';
+                }
+                $nomes_professores = rtrim($nomes_professores, ', '); // Remove trailing comma and space
+
+
+            } else {
+                new TMessage('info', 'Nenhum professor encontrado para esta matéria.');
+            }
+
+            TTransaction::close();
+        } catch (Exception $e) {
+            new TMessage('error', $e->getMessage());
+            TTransaction::rollback();
+        }
+    }
+
 
 }
